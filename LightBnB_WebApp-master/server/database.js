ddf46d91df98afad4769ee59f822b0f9e2c2e934
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-04-07 17:47:44 -0400

    Refractor Get all Properties method to allow for searches

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -26,7 +26,7 @@ const getUserWithEmail = (email) => {
   return pool
 .query(`SELECT * FROM users WHERE email = $1;`, [email])
 .then((result) => {
-  console.log(result.rows[0]);
+
   return result.rows[0];
 })
 .catch ((err) => {
@@ -44,7 +44,7 @@ const getUserWithId = function(id) {
   return pool
   .query(`SELECT * FROM users WHERE id = $1;`, [id])
   .then((result) => {
-    console.log(result.rows[0]);
+
 
     return result.rows[0];
 
@@ -65,7 +65,6 @@ const addUser =  function(user) {
   return pool
   .query(`INSERT INTO users(name, email, password) VALUES ($1, $2, $3) RETURNING *;` ,[user.name, user.email, user.password])
   .then((result) => {
-    console.log(result.rows[0]);
     return result.rows[0];
   })
   .catch ((err) => {
@@ -84,7 +83,8 @@ exports.addUser = addUser;
  */
 const getAllReservations = function(guest_id, limit = 10) {
   return pool
-  .query(`SELECT DISTINCT reservations.*, properties.* FROM reservations
+  .query(`SELECT DISTINCT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+  FROM reservations
   JOIN properties ON reservations.property_id = properties.id
   JOIN property_reviews ON properties.id = property_reviews.property_id
   WHERE reservations.guest_id = $1
@@ -109,16 +109,60 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
- const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      console.log(result.rows);
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+ const getAllProperties = function(options, limit = 10) {
+  // 1 Setup an array to hold any parameters that may be available for the query
+  const queryParams = [];
+  // 2 Start the query with all information that comes before the WHERE clause.
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3 Check if a city, owner_id or price_per_night has been passed in as an option. Add them to the params array and create a WHERE clause
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `AND city LIKE $${queryParams.length} `;
+  }
+
+  // console.log('options', options)
+
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+      queryString += `AND owner_id = $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night){
+    queryParams.push(options.minimum_price_per_night * 100);
+      queryString += `AND cost_per_night >= $${queryParams.length}  `;
+  }
+
+  if(options.maximum_price_per_night) {
+    queryParams.push(   options.maximum_price_per_night * 100);
+    queryString += `AND cost_per_night <= $${queryParams.length}`;
+  }
+
+
+
+  // 4 Add any query that comes after the WHERE clause.
+  queryString += `
+  GROUP BY properties.id
+  `;
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+
+  // 6 Run the query.
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 
 exports.getAllProperties = getAllProperties;
